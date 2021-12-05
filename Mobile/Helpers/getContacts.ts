0e1633def2613a2843c684a8b0886d1ffb3df1e8
Trent Farley <trent.a.farley@gmail.com>
2021-12-05 12:35:12 -0800

    Added procfile

---
+++ Mobile/Helpers/getContacts.ts
@@ -0,0 +1,8 @@
+// async function getContacts(setState){
+//   const { status } = await Contacts.requestPermissionsAsync();
+//   if (status === 'granted') {
+//     const { data } = await Contacts.getContactsAsync(
+//       {
+//         fields: [Contacts.Fields.Name],
+//       });
+// }
